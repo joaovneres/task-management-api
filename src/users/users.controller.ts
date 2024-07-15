5bb0c039909aa7f99fb8ff014f4cf41a402078a6
Neres <joaovneres@outlook.com>
2024-07-15 15:02:22 -0300

    feat: Create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { UserDto } from './user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private usersService: UsersService) {}
+
+    @Post()
+    create(@Body() user: UserDto) {
+        return this.usersService.create(user);
+    }
+}
